cc05618f3dfa04b554254b88f21187c05a6cc7e5
Khairul Haaziq <rhycoz@yahoo.com>
2022-12-31 11:24:45 +0800

    refactor: add button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,29 @@
 <script>
-    import Accordion from "$lib/Accordion/Accordion.svelte";
-	import AccordionItem from "$lib/Accordion/AccordionItem.svelte";
-    import AccordionContent from "$lib/Accordion/AccordionContent.svelte";
-    import AccordionHeader from "$lib/Accordion/AccordionHeader.svelte";
-
+	import Accordion from '$lib/Accordion/Accordion.svelte';
+	import AccordionItem from '$lib/Accordion/AccordionItem.svelte';
+	import AccordionContent from '$lib/Accordion/AccordionContent.svelte';
+	import AccordionHeader from '$lib/Accordion/AccordionHeader.svelte';
+	import DropdownMenu from '$lib/DropdownMenu/DropdownMenu.svelte';
+	import Tooltip from '$lib/Tooltip/Tooltip.svelte';
+	import Button from '$lib/Button/Button.svelte';
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Accordion>
-	<!-- Open -->
-	<AccordionItem open>
-		<svelte:fragment slot="lead">(lead)</svelte:fragment>
-		<svelte:fragment slot="header">Accordion 1</svelte:fragment>
-		<svelte:fragment slot="content">Content 1</svelte:fragment>
-	</AccordionItem>
-	<!-- Closed -->
-	<AccordionItem>
-		<svelte:fragment slot="header">Accordion 2</svelte:fragment>
-	</AccordionItem>
-</Accordion>
-
 <Accordion class="rounded-lg overflow-hidden border border-black">
-    <AccordionItem class="">
-        <AccordionHeader slot="header" class="bg-blue-400 flex p-4">Header2</AccordionHeader>
-        <AccordionContent slot="content" class="bg-red-400 p-4">Hahahha</AccordionContent>
+	<AccordionItem class="">
+		<AccordionHeader slot="header" class="bg-blue-400 flex p-4">Header2</AccordionHeader>
+		<AccordionContent slot="content" class="bg-red-400 p-4">Hahahha</AccordionContent>
 	</AccordionItem>
 	<AccordionItem class="">
-        <AccordionHeader slot="header" class="bg-blue-400 flex p-4">Header2</AccordionHeader>
-        <AccordionContent slot="content" class="bg-red-400 p-4">Hahahha</AccordionContent>
+		<AccordionHeader slot="header" class="bg-blue-400 flex p-4">Header2</AccordionHeader>
+		<AccordionContent slot="content" class="bg-red-400 p-4">Hahahha</AccordionContent>
 	</AccordionItem>
 </Accordion>
+
+<DropdownMenu />
+
+<p>Haha</p>
+<Tooltip />
+
+<Button size="medium">
+Button
+</Button>
