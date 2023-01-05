b9a4a6f833f8214b4651b2b8647844a5abe8afcf
Khairul Haaziq <rhycoz@yahoo.com>
2023-01-06 04:28:24 +0800

    start working on accordion

---
+++ src/lib/Accordion/AccordionRoot.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { writable, type Writable } from 'svelte/store';
+	import { setContext } from 'svelte';
+
+    export let currentExpanded: Writable<string> = writable('');
+    export let defaultExpanded = ''
+    export let className = '';
+
+    setContext('defaultExpanded', defaultExpanded)
+    setContext('currentExpanded', currentExpanded)
+</script>
+
+<div class={className}>
+<slot />
+</div>
